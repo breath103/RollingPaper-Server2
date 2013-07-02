855d0f45f3bb2ed16bb733611ff6e6d3ccb68ba1
breath103 <breath103@gmail.com>
2013-07-03 03:36:53 +0900

    fix url

--- app/models/sound_content.rb
+++ app/models/sound_content.rb
@@ -3,7 +3,7 @@ class SoundContent < ActiveRecord::Base
   belongs_to :user
   belongs_to :paper
   has_attached_file :sound, 
-  	:url => "http://0.0.0.0:3000/:class/:attachment/:id/:style/:basename.:extension",
+  	:url => "http://rollingpaper-production.herokuapp.com/:class/:attachment/:id/:style/:basename.:extension",
     :path => ":rails_root/public/:class/:attachment/:id/:style/:basename.:extension"
   attr_accessible :id, :user_id, :paper_id
   attr_accessible :x, :y, :width, :height, :rotation
