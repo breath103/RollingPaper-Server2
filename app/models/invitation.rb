b08c42697c0c6158e6c34a4801635c177f63a34f
breath103 <breath103@gmail.com>
2013-07-16 00:43:03 +0900

    [#53379035]

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -11,7 +11,17 @@ class Invitation < ActiveRecord::Base
   def accept
     recipient = User.find_by_facebook_id (friend_facebook_id)
     if recipient 
+      #if invitation accepted
+      #send notification to user
       Ticket.create({user_id: recipient.id, paper_id: paper_id})
+      Notification.create({
+        sender: recipient,
+        recipient: sender,
+        notification_type: "invitation_accepted",
+        source_id: paper_id,
+        picture: recipient.picture,
+        text: "#{recipient.username}님이 #{paper.title}에 참여했습니다."
+      })
     end 
   end
   
