8f5677d15b4b5bf1e50111b0de0320013dc128dc
breath103 <breath103@gmail.com>
2013-07-24 00:58:39 +0900

    fix server script

--- server.sh
+++ server.sh
@@ -1,4 +1,5 @@
-sudo bundle exec sidekiq -L ./log/sidekiq.log -d
-rails s -d
-
-
+git checkout .
+git fetch origin
+git pull origin master
+bundle exec sidekiq -L ./log/sidekiq.log -d
+bundle exec rails server -d -e production -p 80
