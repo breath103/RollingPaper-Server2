4bb018c97277a8a610dea90b7d68ac8079784014
Sang Huyn Kurt Lee <breath103@gmail.com>
2013-08-11 02:36:09 +0900

    fix server.sh

--- server.sh
+++ server.sh
@@ -1,5 +1,7 @@
 git checkout .
 git fetch origin
 git pull origin master
-bundle exec sidekiq -L ./log/sidekiq.log -d
+bundle install
+rake db:migrate 
+bundle exec sidekiq -e production -L ./log/sidekiq.log -d
 bundle exec rails server -d -e production -p 80
