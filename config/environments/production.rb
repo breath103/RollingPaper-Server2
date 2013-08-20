d08731a919b98a65cbb393481461b40af946f989
breath103 <breath103@gmail.com>
2013-08-20 21:11:00 +0900

    WIP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,6 +54,10 @@ RollingPaper::Application.configure do
   # Enable threaded mode
   # config.threadsafe!
 
+  # Raise exception on mass assignment protection for Active Record models
+  # config.active_record.mass_assignment_sanitizer = :strict
+  config.active_record.whitelist_attributes = false
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
